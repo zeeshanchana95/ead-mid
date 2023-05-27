9827844169b347d72ec5264eb4df9bb995ee7e14
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-05-27 17:50:52 +0500

    Implemented AnotherUserParticipation and ThanksYouMessage Component

--- src/components_taskA/ThanksYouMessage.js
+++ src/components_taskA/ThanksYouMessage.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { AnotherUserParticipation } from './AnotherUserParticipation'
 
 export const ThanksYouMessage = () => {
+
+const callAnotherParticipationComp = () => {
+    return <AnotherUserParticipation />
+}
   return (
-    <div>ThanksYouMessage</div>
+    <>
+        <div>ThanksYouMessage</div>
+        <button onClick={callAnotherParticipationComp}>Want another participation</button>    
+    </>
+    
   )
 }
